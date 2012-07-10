b1e4cd859cdce5218b25b082de59bd59a28c37aa
Alon Swartz <alon@turnkeylinux.org>
2012-07-10 14:57:00 +0300

    inithook to set mambo admin email and password

---
+++ overlay/usr/lib/inithooks/bin/mambo.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+"""Set Mambo admin password and email
+
+Option:
+    --pass=     unless provided, will ask interactively
+    --email=    unless provided, will ask interactively
+
+"""
+
+import sys
+import getopt
+import hashlib
+
+from dialog_wrapper import Dialog
+from mysqlconf import MySQL
+from executil import system
+
+def usage(s=None):
+    if s:
+        print >> sys.stderr, "Error:", s
+    print >> sys.stderr, "Syntax: %s [options]" % sys.argv[0]
+    print >> sys.stderr, __doc__
+    sys.exit(1)
+
+def main():
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
+                                       ['help', 'pass=', 'email='])
+    except getopt.GetoptError, e:
+        usage(e)
+
+    password = ""
+    email = ""
+    for opt, val in opts:
+        if opt in ('-h', '--help'):
+            usage()
+        elif opt == '--pass':
+            password = val
+        elif opt == '--email':
+            email = val
+
+    if not password:
+        d = Dialog('TurnKey Linux - First boot configuration')
+        password = d.get_password(
+            "Mambo Password",
+            "Enter new password for the Mambo 'admin' account.")
+
+    if not email:
+        if 'd' not in locals():
+            d = Dialog('TurnKey Linux - First boot configuration')
+
+        email = d.get_email(
+            "Mambo Email",
+            "Enter email address for the Mambo 'admin' account.",
+            "admin@example.com")
+
+    hash = hashlib.md5(password).hexdigest()
+
+    m = MySQL()
+    m.execute('UPDATE mambo.users SET password=\"%s\" WHERE username=\"admin\";' % hash)
+    m.execute('UPDATE mambo.users SET email=\"%s\" WHERE username=\"admin\";' % email)
+
+    config = "/var/www/mambo/configuration.php"
+    system("sed -i \"s|mailfrom =.*|mailfrom = '%s';|\" %s" % (email, config))
+
+
+if __name__ == "__main__":
+    main()
+
